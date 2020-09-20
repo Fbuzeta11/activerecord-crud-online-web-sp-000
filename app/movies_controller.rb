1821a8a0552ec43ea01e80ac23444ace630aded8
Federico Buzeta Borquez <fbuzeta11@gmail.com>
2020-09-20 14:47:39 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -17,8 +17,9 @@ end
 
 def can_be_created_in_a_block(args)
   Movie.create do |m|
-    m.title = "The Room"
-    m.release_date = 2003
+    m.title = args[:title]
+   m.release_date = args[:release_date]
+ end
   end
 end
 
